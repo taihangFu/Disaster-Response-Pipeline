974b97024fd4ad2de64a7e358d84ed5de86a8b61
tatsonreload <tatsonreload@icloud.com>
2019-08-20 14:04:40 +0000

    workning on create message cloud image

--- app/run.py
+++ app/run.py
@@ -11,6 +11,7 @@ from plotly.graph_objs import Bar
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
 
+from messagewordcloud import MessageWordCloud
 
 app = Flask(__name__)
 
@@ -26,11 +27,11 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('DisasterResponse', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
@@ -66,12 +67,21 @@ def index():
         }
     ]
     
+    #TODO: visualise wordcloud?? for column MESSAGE
+    message=None    # TODO: extract messages
+    mwc = MessageWordCloud(messages)
+    mwc.preprocess(punctuations=False, decapitalisation=True, stopwords=False)
+    mwc.create_word_cloud_img()
+        #TODO: pass img to index.html
+        
+    #TODO: show example of message of each genre
+    
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
     
     # render web page with plotly graphs
-    return render_template('master.html', ids=ids, graphJSON=graphJSON)
+    return render_template('master.html', ids=ids, graphJSON=graphJSON, word_cloud=word_cloud.png)
 
 
 # web page that handles user query and displays model results
