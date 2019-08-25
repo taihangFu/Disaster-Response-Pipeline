cf1f2a2bb8cc5c11156f3023e703723165234c41
TaiHang_FU <tatsonreload@icloud.com>
2019-08-25 14:43:30 +0800

    add docsting

--- app/text_preprocessing.py
+++ app/text_preprocessing.py
@@ -6,17 +6,19 @@ from nltk import word_tokenize
 from nltk.corpus import stopwords as sw
 from nltk.stem import WordNetLemmatizer
 
-#TODO: stem
 # preprocessing the contents of wiki page of the _word, pre
 def preprocess(contents='', punctuations=False, stopwords=False, lemmatization=False):
     '''
+    Text preprocessing, included punctuations removal, sotpwords removal and lemmatization, and text normalization(lower case, keep alphabet and number only), then count for preprocessed words
     
-    '''
+        Returns:
+            word_frequency: A dictionary contains word and frequency pairs that is generated from preprocessed contents
+        '''
     
     #init
     word_frequency={}
     # Normalize text
-    ##lower case, remove non -alphabet, numeric
+    ##lower case, remove non -alphabet, number
     contents = re.sub(r"[^a-zA-Z0-9]", " ", contents.lower())
 
     # remove punctuations
@@ -25,7 +27,7 @@ def preprocess(contents='', punctuations=False, stopwords=False, lemmatization=F
 
     # remove stop words
     tokens = word_tokenize(contents)
-    cachedStopWords = set(sw.words("english")) #dramatic boost from not having to read the list    from disk every time,, turn to set make another boost
+    cachedStopWords = set(sw.words("english")) #dramatic boost from not having to read the list from disk every time,, turn to set make another boost
     
     if stopwords:
         tokens = [w for w in tokens if w not in cachedStopWords]
